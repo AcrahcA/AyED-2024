e660d6190633168e67d6b8429b0ecff426811930
AcrahcA <167359527+AcrahcA@users.noreply.github.com>
2024-04-25 15:37:47 -0300

    Add files via upload

---
+++ Etapa0/1_Charca_HolaAriel/1_Charca_HolaAriel/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Charca_HolaAriel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cual es tu nombre?");
+            String Name = Console.ReadLine();
+            Console.WriteLine("Hola" + Name);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Etapa0/TestVariables/TestVariables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String cadena = "15,557294";
+            int entero = 15;
+            bool boolean = false;
+            float flotante = 15.557294f;
+            double doble = 15.557294;
+            Char caracter = '1';
+
+            Console.WriteLine("String " + cadena);
+            Console.WriteLine("int " + entero);
+            Console.WriteLine("bool " + boolean);
+            Console.WriteLine("float " + flotante);
+            Console.WriteLine("double " + doble);
+            Console.WriteLine("char " + caracter);
+
+            entero = int.Parse(Console.ReadLine());
+
+            Console.ReadKey();
+
+        }
+    }
+}
