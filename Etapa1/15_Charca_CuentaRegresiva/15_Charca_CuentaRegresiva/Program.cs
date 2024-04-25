769a7ada11f58bad97a1b4c40b98fdc9bf82e2cc
AcrahcA <167359527+AcrahcA@users.noreply.github.com>
2024-04-25 16:16:48 -0300

    Add files via upload

---
+++ Etapa1/15_Charca_CuentaRegresiva/15_Charca_CuentaRegresiva/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Charca_CuentaRegresiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dame un numero positivo");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("----------------------------------");
+            for (int i = 0; i < num + 1; i++)
+            {
+                Console.WriteLine(num-i);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
