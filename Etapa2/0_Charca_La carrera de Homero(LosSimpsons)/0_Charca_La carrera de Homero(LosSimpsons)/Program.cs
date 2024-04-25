e7037886b09e169b7e6ee700740a46c8488d28dc
AcrahcA <167359527+AcrahcA@users.noreply.github.com>
2024-04-25 17:09:39 -0300

    Add files via upload

---
+++ Etapa2/0_Charca_La carrera de Homero(LosSimpsons)/0_Charca_La carrera de Homero(LosSimpsons)/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Charca_La_carrera_de_Homero_LosSimpsons_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresa la cantidad de obstáculos.");
+            int obstaculos =int.Parse( Console.ReadLine());
+            int[] losObstaculos = new int[obstaculos];
+            String si = "si";
+            String no = "no";
+            int puntos = 0;
+            for (int i=0; i < obstaculos; i++)
+            {
+                Console.WriteLine("Homero supera el Obstaculo "+(i+1));
+                String respuesta = Console.ReadLine();
+                if (respuesta == si)
+                {
+                    losObstaculos[i] = 10;
+                }
+                else if(respuesta==no)
+                {
+                    losObstaculos[i] = - 5;
+                }
+                else if(!(respuesta==si || respuesta==no))
+                {
+                    Console.WriteLine("Homero supera el Obstaculo " + (i + 1));
+                    String respuesta2 = Console.ReadLine();
+                    if (respuesta2 == si)
+                    {
+                        losObstaculos[i] = 10;
+                    }
+                    else if (respuesta2 == no)
+                    {
+                        losObstaculos[i] = -5;
+                    }
+
+                }
+                puntos = puntos + losObstaculos[i];
+
+            }
+            Console.WriteLine("homero gano "+ puntos + " puntos");
+            Console.ReadKey();
+
+
+        }
+    }
+}
