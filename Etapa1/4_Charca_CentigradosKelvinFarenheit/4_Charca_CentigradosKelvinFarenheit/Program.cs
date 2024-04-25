1cec8711cacffd976f041034544d440eee409139
AcrahcA <167359527+AcrahcA@users.noreply.github.com>
2024-04-25 15:45:54 -0300

    Add files via upload

---
+++ Etapa1/4_Charca_CentigradosKelvinFarenheit/4_Charca_CentigradosKelvinFarenheit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Charca_CentigradosKelvinFarenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresa una cantidad de grados pare convertirlos en Kelvin y Farenheit");
+            int grados = int.Parse(Console.ReadLine());
+            Console.WriteLine("Kelvin:" + (grados + 273));
+            Console.WriteLine("Farenheit:" + ((grados * 18)/10)+ 32);
+            Console.ReadKey();        }
+    }
+}
