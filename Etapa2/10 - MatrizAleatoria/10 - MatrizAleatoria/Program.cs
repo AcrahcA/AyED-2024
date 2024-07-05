9a3df20eaf054f8727729c02a549c5f39997a0ae
Acrahc A <167359527+AcrahcA@users.noreply.github.com>
2024-07-05 16:56:36 -0300

    Add files via upload

---
+++ Etapa2/10 - MatrizAleatoria/10 - MatrizAleatoria/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10___MatrizAleatoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ramdom = new Random();
+            Console.Write("ingresa una cantidad de filas ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("ingresa una cantidad de columnas ");
+            int m = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matriz[i, j] = ramdom.Next(1,100);
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine("");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
