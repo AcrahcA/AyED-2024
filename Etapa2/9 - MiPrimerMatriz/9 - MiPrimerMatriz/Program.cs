2eb946ba1cf5d1debbf9cf7c043d2b62e6ba3a71
Acrahc A <167359527+AcrahcA@users.noreply.github.com>
2024-07-05 16:45:30 -0300

    Add files via upload

---
+++ Etapa2/9 - MiPrimerMatriz/9 - MiPrimerMatriz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9___MiPrimerMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingresa una cantidad de filas ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("ingresa una cantidad de columnas ");
+            int m = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    int r = int.Parse(Console.ReadLine());
+                    matriz[i, j] = r;
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine("");
+
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
